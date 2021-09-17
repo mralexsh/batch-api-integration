6b02a88238dc56489e2d7b0edd595f76a81ade66
Aleksey Shimonchuk <a.shimonchuk@sourcecode.ru>
2021-09-17 14:58:31 +0700

    Setup basic Camel project

--- src/main/java/com/integration/batch/BatchApplication.java
+++ src/main/java/com/integration/batch/BatchApplication.java
@@ -2,7 +2,9 @@ package com.integration.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource({"classpath:application-context.xml"})
 @SpringBootApplication
 public class BatchApplication {
 
